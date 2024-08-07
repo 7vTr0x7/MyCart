8d93630e83a3ecf0b4f538c36b0471c0aeb171c8
7vTr0x7 <7vTrox7@gmail.com>
2024-08-07 19:09:23 +0530

    frontend - pages - Products - features - ProductsSection

--- Frontend/src/pages/products/features/ProductsSection.jsx
+++ Frontend/src/pages/products/features/ProductsSection.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { CiHeart } from "react-icons/ci";
 const ProductsSection = ({ products }) => {
   return (
     <div className="bg-body-tertiary py-4 px-5 ">
@@ -12,29 +12,34 @@ const ProductsSection = ({ products }) => {
         {products &&
           products.map((prod) => (
             <div key={prod._id} className="col-md-3">
-              <div className="card rounded-0 mt-2">
-                <div className="">
+              <div className="card rounded-0 mt-3 ">
+                <div>
+                  <span
+                    className="fs-4 mx-2"
+                    style={{ position: "absolute", right: "0" }}>
+                    <CiHeart />
+                  </span>
                   <img
-                    className="img-fluid h-50 w-100"
+                    className="img-fluid card-img-top"
                     alt={prod.name}
                     src={`${prod.imageUrl}${prod.name}`}
                   />
                 </div>
                 <div className="card-body p-0 text-center">
                   <div>
-                    <small className="px-0 py-2 m-0">
+                    <p className="px-0 pt-2 m-0">
                       <b>{`${
-                        prod.name.length > 10
-                          ? `${prod.name.slice(0, 10)} ...`
+                        prod.name.length > 15
+                          ? `${prod.name.slice(0, 15)} ...`
                           : prod.name
                       }`}</b>
-                    </small>
+                    </p>
                     <p className="px-0 pt-1 pb-3  m-0">
                       <b>${prod.price}</b>
                     </p>
                   </div>
                   <div className="sticky-bottom">
-                    <button className="btn btn-outline-secondary rounded-0 w-100   fw-bold">
+                    <button className="btn btn-outline-secondary rounded-0 w-100 border-0  fw-bold">
                       Add to Cart
                     </button>
                   </div>
