6a8dd9394bdd05d2f98161fe79d3a77d663272b7
7vTr0x7 <7vTrox7@gmail.com>
2024-08-07 16:12:20 +0530

    frontend - Header

---
+++ Frontend/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="row container ">
+        <div className="col-md-4">
+          <h3>MyCart</h3>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
