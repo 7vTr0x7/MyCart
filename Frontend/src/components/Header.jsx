e70aecf6323e23eff2da101394491969c6831243
7vTr0x7 <7vTrox7@gmail.com>
2024-08-07 16:32:45 +0530

    frontend - Header

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { MdOutlineShoppingCart } from "react-icons/md";
+import { FaRegHeart } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <header>
-      <div className="row container ">
-        <div className="col-md-4">
-          <h3>MyCart</h3>
+    <header className="container">
+      <div className="row py-3">
+        <div className="col-md-4 pt-2">
+          <h4>MyCart</h4>
+        </div>
+        <div className="col-md-4 pt-1">
+          <input type="text" placeholder={"Search"} className="form-control" />
+        </div>
+        <div className="col-md-4 text-end">
+          <span className="px-3 fs-4">
+            <FaRegHeart />
+          </span>
+          <span className="px-3 fs-3">
+            <MdOutlineShoppingCart />
+          </span>
         </div>
       </div>
     </header>
