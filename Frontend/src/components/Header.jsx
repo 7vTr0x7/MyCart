64f784fa5c5a05fb21c971259bcbdd32da742d92
7vTr0x7 <7vTrox7@gmail.com>
2024-08-12 16:58:37 +0530

    frontend - components - Header

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { FaRegHeart } from "react-icons/fa";
+import { IoBagHandleOutline } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+
 import { Link } from "react-router-dom";
 
 const Header = () => {
@@ -18,11 +21,17 @@ const Header = () => {
           <input type="text" placeholder={"Search"} className="form-control" />
         </div>
         <div className="col-md-4 text-end">
+          <span className="px-3 fs-3">
+            <IoBagHandleOutline />
+          </span>
           <span className="px-3 fs-4">
             <FaRegHeart />
           </span>
           <span className="px-3 fs-3">
             <MdOutlineShoppingCart />
+          </span>
+          <span className="px-3 fs-3">
+            <CgProfile />
           </span>
         </div>
       </div>
