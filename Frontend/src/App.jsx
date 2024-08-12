b1141ebdf49e9ac8fff8390c8ee03cc65ad3bba7
7vTr0x7 <7vTrox7@gmail.com>
2024-08-12 17:26:27 +0530

    frontend - pages - Products - ProductDetails

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/home/Home";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Products from "./pages/products/Products";
+import ProductDetails from "./pages/products/ProductDetails";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
+          <Route path="/productDetails" element={<ProductDetails />} />
         </Routes>
       </Router>
     </>
