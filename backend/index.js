fc1ae2b21e8091c4f3aaf9393450f5f2f414c3fa
7vTr0x7 <7vTrox7@gmail.com>
2024-08-19 13:57:59 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: "*",
+  origin: "http://localhost:5173",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
