609e33ec4fb89b0fbcf0ba3509d804fb9778a55f
7vTr0x7 <7vTrox7@gmail.com>
2024-08-19 17:02:49 +0530

    backend

--- backend/db/db.connect.js
+++ backend/db/db.connect.js
@@ -1,7 +1,9 @@
 require("dotenv").config({ path: "D:/mycart/backend/.env" });
 const mongoose = require("mongoose");
 
-const mongoUrl = process.env.MONGODB;
+const mongoUrl =
+  process.env.MONGODB ||
+  "mongodb+srv://neoGStudent:vTroxGAMING@neog.vf9x0bo.mongodb.net/?retryWrites=true&w=majority&appName=neoG";
 
 const initializeDatabase = async () => {
   try {
