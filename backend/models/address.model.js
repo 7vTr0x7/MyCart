0f4422e16b8164b21907cadd020f9d2c002af333
7vTr0x7 <7vTrox7@gmail.com>
2024-08-20 15:36:24 +0530

    backend - models - address.model

---
+++ backend/models/address.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const addressSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  houseNo: { type: String, required: true },
+  street: { type: String, required: true },
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+  country: { type: String, required: true },
+  postalCode: { type: String, required: true },
+  mobileNumber: { type: Number, required: true },
+});
+
+const Addresses = mongoose.model("Addresses", addressSchema);
+
+module.exports = Addresses;
