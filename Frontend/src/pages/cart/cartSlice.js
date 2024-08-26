7f1e4b1d816a7356250ee7ce3ee88a492f917fb9
7vTr0x7 <7vTrox7@gmail.com>
2024-08-26 15:35:02 +0530

    frontend - pages - cart - cartSlice

--- Frontend/src/pages/cart/cartSlice.js
+++ Frontend/src/pages/cart/cartSlice.js
@@ -1,4 +1,31 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const addToCart = createAsyncThunk(
+  "addToCart",
+  async ({ userId, prodId }) => {
+    try {
+      const res = await fetch(
+        `https://mycartbackend.vercel.app/api/users/user/${userId}/cart`,
+        {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify({ prodId }),
+        }
+      );
+
+      if (!res.ok) {
+        console.log("Failed to add to cart");
+      }
+
+      const data = await res.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
 
 const cartSlice = createSlice({
   name: "cart",
@@ -7,31 +34,6 @@ const cartSlice = createSlice({
     productIds: [],
   },
   reducers: {
-    addToCart: (state, action) => {
-      const exists = state.cart.find((prod) => prod._id == action.payload._id);
-      if (!exists) {
-        return {
-          ...state,
-          cart: [...state.cart, { ...action.payload, quantity: 1 }],
-          productIds: [...state.productIds, action.payload._id],
-        };
-      } else {
-        const index = state.cart.findIndex(
-          (prod) => prod._id === action.payload
-        );
-        state.cart[index] = {
-          ...state.cart[index],
-          quantity: state.cart[index].quantity + 1,
-        };
-      }
-    },
-    removeFromCart: (state, action) => {
-      return {
-        ...state,
-        cart: state.cart.filter((prod) => prod._id != action.payload),
-        productIds: state.productIds.filter((id) => id != action.payload),
-      };
-    },
     incQuantity: (state, action) => {
       const index = state.cart.findIndex((prod) => prod._id === action.payload);
 
@@ -53,7 +55,6 @@ const cartSlice = createSlice({
   },
 });
 
-export const { addToCart, removeFromCart, incQuantity, decQuantity } =
-  cartSlice.actions;
+export const { removeFromCart, incQuantity, decQuantity } = cartSlice.actions;
 
 export default cartSlice.reducer;
