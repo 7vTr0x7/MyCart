7c8df0e5bced683cf19ffca03e9049a746380393
7vTr0x7 <7vTrox7@gmail.com>
2024-08-26 13:10:59 +0530

    frontend - pages - wishlist - wishlistSlice

--- Frontend/src/pages/wishlist/wishlistSlice.js
+++ Frontend/src/pages/wishlist/wishlistSlice.js
@@ -1,38 +1,58 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const addToWishlist = createAsyncThunk(
+  "addToWishlist",
+  async ({ userId, prodId }) => {
+    try {
+      const res = await fetch(
+        `https://mycartbackend.vercel.app/api/users/user/${userId}/wishlist`,
+
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ prodId }),
+        }
+      );
+
+      if (!res.ok) {
+        console.log("Failed to add to wishlist");
+      }
+
+      const data = await res.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
 
 const wishlistSlice = createSlice({
   name: "wishlist",
   initialState: {
     wishlist: [],
     wishlistProductIds: [],
+    status: "idle",
+    error: null,
   },
-  reducers: {
-    addToWishlist: (state, action) => {
-      const exists = state.wishlist.find(
-        (prod) => prod._id == action.payload._id
-      );
-      if (!exists) {
-        return {
-          ...state,
-          wishlist: [...state.wishlist, action.payload],
-          wishlistProductIds: [...state.wishlistProductIds, action.payload._id],
-        };
-      }
-    },
-    removeFromWishlist: (state, action) => {
-      return {
-        ...state,
-        wishlist: state.wishlist.filter(
-          (prod) => prod._id != action.payload._id
-        ),
-        wishlistProductIds: state.wishlistProductIds.filter(
-          (id) => id != action.payload._id
-        ),
-      };
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(addToWishlist.pending, (state, action) => {
+      state.status = "Loading";
+    });
+    builder.addCase(addToWishlist.fulfilled, (state, action) => {
+      state.status = "Success";
+
+      state.wishlistProductIds = [...state.wishlistProductIds, action.payload];
+    });
+    builder.addCase(addToWishlist.rejected, (state, action) => {
+      state.status = "failed";
+      state.error = "failed to add to wishlist";
+    });
   },
 });
 
-export const { addToWishlist, removeFromWishlist } = wishlistSlice.actions;
+export const { removeFromWishlist } = wishlistSlice.actions;
 
 export default wishlistSlice.reducer;
